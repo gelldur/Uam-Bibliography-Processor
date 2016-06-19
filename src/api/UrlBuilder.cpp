9fff2b9d7f2aa65af7cdf46781c1946e6fddd401
Dawid Drozd <drozddawid.uam@gmail.com>
2016-06-19 23:39:59 +0200

    Add UrlBuilder

---
+++ src/api/UrlBuilder.cpp
@@ -0,0 +1,33 @@
+//
+// Created by Dawid Drozd aka Gelldur on 6/18/16.
+//
+
+#include "UrlBuilder.h"
+
+#include "UrlBuilder.h"
+
+UrlBuilder::UrlBuilder()
+{
+}
+
+UrlBuilder::UrlBuilder(const std::string& url)
+{
+	appendUrl(url);
+}
+
+UrlBuilder& UrlBuilder::appendUrl(const std::string& string)
+{
+	_builder.setPath(_builder.getPath() + string);
+	return *this;
+}
+
+UrlBuilder& UrlBuilder::appendUrl(const long long value)
+{
+	_builder.setPath(_builder.getPath() + std::to_string(value));
+	return *this;
+}
+
+std::string UrlBuilder::build()
+{
+	return _builder.toString();
+}
